c955f7dd874a1c08bb5c3aa8e6a3970263d4c011
Christian Merrill <christian.j.merrill@gmail.com>
2023-04-30 09:47:42 -0400

    Update communication.py

--- paperpal/communication.py
+++ paperpal/communication.py
@@ -3,30 +3,82 @@ import json
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import datetime
 
 class GmailCommunication:
 
-    def __init__(self, sender_address=None, app_password=None, creds_path=None):
+    def __init__(self, sender_address=None, reciever_address=None, app_password=None, creds_path=None):
         self.sender_address = sender_address
         self.app_password = app_password
+        self.reciever_address = reciever_address
+        self.email_message = None
+        
         if creds_path:
             try:
                 with open(creds_path, 'r') as f:
                         self.json_creds = json.load(f)
             except Exception as e:
-                 raise Exception(f"Unable to read credentials path with error: {e}")
+                 raise Exception(f"Unable to read credentials path with error: {str(e)}")
 
         if not self.app_password:
             self.app_password = os.getenv['app_password']
-            if not self.app_password and creds_path:
+            if not self.app_password:
+                if not creds_path:
+                    raise Exception("No application password found.  Please pass an application password or check your json for an app_password key.")
                 self.app_password = self.json_creds.get('app_password', None)
                 if not self.app_password:
                     raise Exception("No application password found.  Please pass an application password or check your json for an app_password key.")
                 
-        if not self.sender_address and creds_path:
-            self.sender_address = self.json_creds.get('sender_address', None)
+        if not self.sender_address:
+            self.sender_address = os.getenv['sender_address']
             if not self.sender_address:
-                raise Exception("No sender address found")
-             
-                
+                if not creds_path:
+                    raise Exception("No sender address found.  Please pass a sender address or check your json for sender_address key.")
+                self.sender_address = self.json_creds.get('sender_address', None)
+                if not self.sender_address:
+                    raise Exception("No sender address found.  Please pass a sender address or check your json for sender_address key.")
+
+
+    def compose_message(self, content):
+        """Method to compose a MIMEMultipart Message
+
+        Args:
+            content (str): string email content for generating an email from.
+        """
+        sender_address = self.sender_address
+        reciever_address = self.reciever_address
+        today = datetime.datetime.today()
+        today = today.strftime('%B %d, %Y')
+        
+        if not reciever_address:  # we send the email to ourselves if we aren't sending it to someone else.
+            reciever_address = sender_address
+        
+        message = MIMEMultipart()
+        message["From"] = sender_address
+        message["To"] = reciever_address
+        message['Subject'] = f"PaperPal Paper Assessment for {today}"
+        message.attach(MIMEText(content, 'plain'))
+        self.email_message = message
+    
+
+    def send_email(self):
+
+        sender_address = self.sender_address
+        reciever_address = self.reciever_address
+        
+        if not reciever_address:
+            reciever_address = sender_address
+        
+        app_password = self.app_password
+        message = self.email_message
+        
+        try:
+            session = smtplib.SMTP('smtp.gmail.com', 587)
+            session.starttls()
+            session.login(sender_address, app_password)
+            message_text  = message.as_string()
+            session.sendmail(sender_address, reciever_address, message_text)
+            session.quit()
+        except Exception as e:
+            raise Exception(f"Unable to send email with exception {str(e)}")
                 
