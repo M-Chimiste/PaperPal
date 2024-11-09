67365609280cfac292e22a4d8ce4d9f13881a70d
M Chimiste <37678080+M-Chimiste@users.noreply.github.com>
2024-11-09 08:45:19 -0500

    Update __init__.py

--- paperpal/prompt/__init__.py
+++ paperpal/prompt/__init__.py
@@ -1,6 +1,8 @@
 from .prompts import (research_interests_prompt, 
                      newsletter_prompt,
                      research_prompt,
+                     newsletter_context_prompt,
+                     newsletter_final_prompt,
                      RESEARCH_INTERESTS_SYSTEM_PROMPT,
                      NEWSLETTER_SYSTEM_PROMPT)
 from .prompting import prompt
